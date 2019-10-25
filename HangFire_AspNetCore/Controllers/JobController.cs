ed8cbd46ce6b49514ebbd95997c3334d8aba4b50
João Mannes <joao.mannes@rovitex.com.br>
2019-10-25 09:48:56 -0300

    Criação do projeto

---
+++ HangFire_AspNetCore/Controllers/JobController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFire_AspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var jobFireForget = BackgroundJob.Enqueue(() => Console.WriteLine($"Disparar agora: {DateTime.Now}"));
+            var jobDelayed = BackgroundJob.Schedule(() => Console.WriteLine($"Com Delay: {DateTime.Now}"), TimeSpan.FromSeconds(10));
+            BackgroundJob.ContinueJobWith(jobDelayed, () => Console.WriteLine($"Continuação: {DateTime.Now}"));
+            RecurringJob.AddOrUpdate(() => Console.WriteLine($"Job Recorrente: {DateTime.Now}"), Cron.Minutely);
+
+            return Ok("Jobs criados com sucesso");
+        }
+    }
+}
---
+++ HangFire_AspNetCore/Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HangFire_AspNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(
+                h => h.UseSqlServerStorage(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Hangfire_Aspnet;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+            );
+
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+
+            app.UseMvc();
+        }
+    }
+}
